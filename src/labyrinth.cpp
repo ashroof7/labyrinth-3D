b3e72c0ba6d341f6b2a78af2a800da7f367a2c7a
leonardo7 <leo_aly7@ymail.com>
2013-12-20 16:08:35 +0200

    drawing a single debugging cube

--- src/labyrinth.cpp
+++ src/labyrinth.cpp
@@ -1,4 +1,6 @@
 #include "include/Angel.h"
+#include "cube.h"
+
 #include <iostream>
 #include <time.h>
 #include <sys/time.h>
@@ -10,7 +12,10 @@ using namespace Angel;
 
 GLfloat screen_width = 600;
 GLfloat screen_height = 600;
+
 GLuint program;
+GLuint vao;
+
 GLuint P_loc, M_loc, V_loc, W_loc; // locations pointers of P, M, V, W matrices in shader
 
 mat4 I_mat = mat4(1.0); // Identity matrix
@@ -18,6 +23,11 @@ mat4 M_mat = mat4(1.0); // Model matrix
 mat4 V_mat = mat4(1.0); // View matrix
 mat4 W_mat = mat4(1.0); // World matrix (rotation of cube by mouse)
 mat4 P_mat = mat4(1.0); // projection matrix
+
+vec3 eye(5,5,-10);
+
+// test cube for EPIC RADO :D :D
+cube *test_cube;
 
 //==================
 //openGL functions
@@ -40,6 +50,9 @@ inline void create_buffer(GLuint* vbo, size_t pts_size, const GLvoid * pts, size
 
 void init_buffers() {
 	// Initializing  VAOs and VBOs
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	test_cube = new cube(program, 0 );
 }
 
 
@@ -49,14 +62,41 @@ void init(void) {
 	program = InitShader("vshader.glsl", "fshader.glsl");
 	glUseProgram(program);
 
-	glClearColor(1.0, 1.0, 1.0, 1.0); // white background
+	init_buffers();
+
+	V_mat = LookAt(eye, vec3(0,0,0), vec3(0,1,0));
+	P_mat = Perspective(45.0f, 1.0f*screen_width/screen_height, 1.0f, 100.0f);
+
+	// matrices location in shader
+	P_loc = glGetUniformLocation(program, "P");
+	V_loc = glGetUniformLocation(program, "V");
+	M_loc = glGetUniformLocation(program, "M");
+	W_loc = glGetUniformLocation(program, "W");
+
+	glUniformMatrix4fv(P_loc, 1, true, P_mat);
+	glUniformMatrix4fv(V_loc, 1, true, V_mat);
+	glUniformMatrix4fv(M_loc, 1, true, M_mat);
+	glUniformMatrix4fv(W_loc, 1, true, W_mat);
+
+	glClearColor(0.9, 0.9, 0.9, 1.0); // grey background :/ not sure about the color
+
+	glEnable(GL_DEPTH_TEST);// Enable depth test
+	glDepthFunc(GL_LESS);// Accept fragment if it closer to the camera than the former one
 }
 
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);     // clear the window//
 
-	glFlush();
+	glEnableVertexAttribArray(vao);
+	glBindVertexArray(vao);
+
+	// update rotation and translation matrices of each object before uploading to shader
+	glUniformMatrix4fv(M_loc, 1, true, M_mat);
+	test_cube->draw();
+
+	glDisableVertexAttribArray(vao);
+	glutSwapBuffers(); // Double buffering
 }
 
 
